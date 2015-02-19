5b6cb0530b08d3b257c089e6538e57f56aa8bb2e
Guilherme Bayma <gfbayma@gmail.com>
2015-02-19 08:54:45 -0200

    Aterado a autoria do projeto, em diversas classes

--- ContaPessoas/AppDelegate.h
+++ ContaPessoas/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Guilherme Bayma on 2/18/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- ContaPessoas/FirstViewController.m
+++ ContaPessoas/FirstViewController.m
@@ -2,8 +2,8 @@
 //  FirstViewController.m
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Guilherme Bayma on 2/18/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
 //
 
 #import "FirstViewController.h"
